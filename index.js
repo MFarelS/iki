d73460aabdefa788248f9bb4c234989a9c941a22
Rizky979 <76403086+Rizky979@users.noreply.github.com>
2020-12-22 11:55:25 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ client.on('group-participants-update', async (anu) => {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["6285722553839@s.whatsapp.net"] // replace this with your number
+			const ownerNumber = ["6282387804410@s.whatsapp.net"] // replace this with your number
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
